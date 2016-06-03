4662f06625b1862fa5ed352d8091aad05de3e99c
quicotte <quicotte@gmail.com>
2016-06-03 16:29:15 +0200

    Commit n° 3

--- src/main/java/streaming/entity/Film.java
+++ src/main/java/streaming/entity/Film.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+Je suis passé par là !
  */
 package streaming.entity;
 
